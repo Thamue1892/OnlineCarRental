2f0f455a87211797262a9aa1a309b9de5dd6b4f0
ThaymesCoder <tmpofu@thami.co.za>
2018-12-15 14:42:21 +0200

    Add project files.

---
+++ ThaymesCabs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThaymesCabs.MvcApplication" Language="C#" %>
---
+++ ThaymesCabs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ThaymesCabs.Startup))]
+namespace ThaymesCabs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
